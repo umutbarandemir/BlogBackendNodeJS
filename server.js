2f45f0153e1fb88136e2ba0f4cc637fe734993dc
umutbarandemir <baran292907@hotmail.com>
2025-01-19 18:06:54 +0300

    Cards created, Routing added, Sillydata

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const articleRouter = require('./routes/articles');
 const app = express();
 
 app.set('view engine','ejs');
 
+app.use('/articles',articleRouter)
+
 app.get("/",(req,res)=>{
     // res.send("ServerStarted!!!");
-    res.render('index');
+
+    const articles = [{
+        title: "Test Article",
+        createdAt : new Date(),
+        description : "Test Description"
+    },{
+        title: "Test Article222",
+        createdAt : new Date(),
+        description : "Test Description222"
+    }]
+    res.render('index',{articles : articles});
 });
 
 app.listen(5000);
