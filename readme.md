4f561c8e8b6cb1b915d1784b0f0fbe6111fc62d5
umutbarandemir <baran292907@hotmail.com>
2025-02-01 17:01:05 +0300

    slugify and marked added, slug done, delete button added

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 <!-- npm init -y
 npm i express mongoose ejs
-npm i --save-dev nodemon -->
+npm i --save-dev nodemon 
+npm i marked slugify
+ npm i method-override
+-->
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Article = require('./modules/article');
 const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
 const app = express();
 
 mongoose.connect('mongodb://localhost/blog');
 
 app.set('view engine','ejs');
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 
 app.get("/",async(req,res)=>{
