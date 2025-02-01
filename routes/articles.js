82dafd70f5f45588dd613d9397d3d9aae79cbbaa
umutbarandemir <baran292907@hotmail.com>
2025-02-01 17:38:14 +0300

    Edit function and route added

--- routes/articles.js
+++ routes/articles.js
@@ -6,6 +6,11 @@ router.get('/new',(req,res)=>{
     res.render("articles/new",{article : new Article()})
 })
 
+router.get('/edit/:id',async(req,res)=>{
+    const article = await Article.findById(req.params.id)
+    res.render("articles/edit",{article : article})
+})
+
 router.get('/:slug',async(req,res)=>{
     const article = await Article.findOne({slug: req.params.slug})
     if(article === null){
@@ -29,6 +34,24 @@ router.post('/',async(req,res) =>{
         res.render("articles/new", { article: article })
       }
     
+})
+
+router.put("/:id",async(req,res)=>{
+    let article = await Article.findById(req.params.id);
+
+    if (!article) return res.redirect("/");
+
+    article.title = req.body.title;
+    article.description = req.body.description;
+    article.markdown = req.body.markdown;
+
+    try {
+        article = await article.save()
+        res.redirect(`/articles/${article.slug}`) 
+      } catch (e) {
+        console.log(e);
+        res.render("articles/edit", { article: article })
+      }
 })
 
 router.delete("/:id",async(req,res)=>{
